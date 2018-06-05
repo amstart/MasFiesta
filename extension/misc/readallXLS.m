e18cabb9539e8c691ef5d50836afad98742e817d
amstart <jochen.krattenmacher@web.de>
2018-06-05 21:52:50 +0200

    removed unrelated utils

--- extension/misc/readallXLS.m
+++
@@ -1,9 +0,0 @@
-columns = 'A:T';
-file = 'Y:\Jochen\180412_flushOutAfterDifferentTime\180412.xlsx';
-[~, worksheets] = xlsfinfo(file);
-% worksheets = {'19_400x_75mM_2min_shrink'};
-objects = struct([]);
-for sheet = worksheets
-    objects(end+1).Results = xlsread(file,sheet{1},columns);
-    objects(end).File = sheet{1};
-end
