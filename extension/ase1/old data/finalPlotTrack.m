504a19348435e3641279c79202be93298e946bea
amstart <jochen.krattenmacher@web.de>
2021-01-11 14:48:09 +0100

    visualize new data sweeping

--- extension/ase1/old data/finalPlotTrack.m
+++ extension/ase1/old data/finalPlotTrack.m
@@ -10,10 +10,10 @@
 % 
 % itracemat = alltrace(5:end-10, :);
 iperm = 172;
-% for i = 1:length(signals)
-signal = Image(110:126,:);
+for i = 1:length(measuredLines)
+signal = Image(round(min(measuredLines(i).Y)-4:max(measuredLines(i).Y)),round(min(measuredLines(i).X):max(measuredLines(i).X)));
 itracemat = double(signal(5:end,:))./iperm;
-bg = mean(signal(1:5,:))./iperm;
+bg = mean(signal(1:4,:))./iperm;
 x = 0:157/4:(size(itracemat,2)*157/4)-1;
 
 figure
@@ -29,7 +29,7 @@ select = ones(size(itracemat,1),1);%track.GFPTip<-500 | isnan(track.GFPTip);
 
 colors = cool(sum(select));
 for iframe = find(select)'
-    itrace = itracemat(iframe,:)./ 64.54;%157
+    itrace = (itracemat(iframe,:)-bg)./ 64.54;%157
 %     if ~isnan(track.tags(iframe+4)) || isnan(track.GFPTip(iframe))
 %         continue
 %     end
@@ -45,13 +45,14 @@ for iframe = find(select)'
 %         'Color', colors(iframe,:));
 end
 colormap('cool');
-time = track.Data2(find(select, 1, 'last'),1) - track.Data2(1,1);
+% time = track.Data2(find(select, 1, 'last'),1) - track.Data2(1,1);
+time = size(itracemat,1)*5;
 c = colorbar('Ticks',[0,1], 'TickLabels',{'0',num2str(time,2)});
-c.Label.String = 'time after catastrophe [s]';
-ylabel('Ase1 density [molecules/nm]')
-xlabel('Distance from seed [nm]')
-pbaspect([2 1 1]);
+% c.Label.String = 'time after catastrophe [s]';
+% ylabel('Ase1 density [molecules/nm]')
+% xlabel('Distance from seed [nm]')
+% pbaspect([2 1 1]);
 set(gca, 'FontSize', 14)
 % legend([bgline fitline nofitline], {'Ase1 profile before catastrophe', 'Ase1 profile at MT tip (used for fitting)', 'Ase1 profile beyond MT tip (not used for fitting)'});
 % xlim([-1000 0])
-% end
+end
